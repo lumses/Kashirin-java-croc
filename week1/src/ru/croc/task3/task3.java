f0f4848b1d46e1d86dcf766334ff5d31a9aae9b9
lumses <90564405+lumses@users.noreply.github.com>
2021-10-19 23:40:15 +0300

    Исправил task3

--- week1/src/ru/croc/task3/task3.java
+++ week1/src/ru/croc/task3/task3.java
@@ -2,39 +2,59 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    // считывание массива, сначала считываем как строку, создаем массив из строк,разделенных пробелов, с помощью parseInt преобразуем строку в число
+    public static int[] readArray(){
+        Scanner console = new Scanner(System.in);
+        String s = console.nextLine();
+        String sArr[] = s.split(" ");
+        int array[] = new int[sArr.length];
+        for (int i = 0; i < sArr.length; i++){
+            array[i] = Integer.parseInt(sArr[i]);
+        }
+        return array;
+    }
 
-    Scanner console = new Scanner(System.in);
-    String s = console.nextLine();
-    String sArr[] = s.split(" ");
-    int array[] = new int[sArr.length];
-    for (int i = 0; i < sArr.length; i++){
-        array[i] = Integer.parseInt(sArr[i]);
-    }
-    int min_elem = array[0];
-    int min_idx = 0;
-    int max_elem = array[0];
-    int max_idx = 0;
-    for (int i = 1; i < array.length; i++){
-        if (array[i] > max_elem){
-            max_elem = array[i];
+    // нахождение индекса максимального элемента в массиве
+    public static int idxMax(int [] array){
+        int max_idx = 0;
+        for (int i = 1; i < array.length; i++){
+        if (array[i] > array[max_idx]){
             max_idx = i;
+            }
         }
-        if (array[i] < min_elem){
-            min_elem = array[i];
+        return max_idx;
+    }
+
+    // нахождение индекса минимального элемента в массиве
+    public static int idxMin(int [] array){
+        int min_idx = 0;
+        for (int i = 1; i < array.length; i++){
+        if (array[i] < array[min_idx]){
             min_idx = i;
+            }
         }
+        return min_idx;
     }
-    int t;
-    t = array[0];
-    array[0] = min_elem;
-    array[min_idx] = t;
-    t = array[array.length-1];
-    array[array.length-1] = max_elem;
-    array[max_idx] = t;
-    for (int i = 0; i < array.length; i++){
-        System.out.print(array[i]+" ");
+
+    // перестановка элементов массива
+    public static void swap(int[] array, int one_idx, int two_idx){
+        int t;
+        t = array[one_idx];
+        array[one_idx] = array[two_idx];
+        array[two_idx] = t;
     }
-    System.out.println();
+    
+    // вывод элементов массива
+    public static void print_array(int[] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int array[] = readArray();
+        swap(array,idxMin(array),0);
+        swap(array,idxMax(array),array.length-1);
+        print_array(array);
     }
 }
