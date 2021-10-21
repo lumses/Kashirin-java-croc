9c4c2c2523cd90ac00655946587676abbd0781d8
lumses <90564405+lumses@users.noreply.github.com>
2021-10-21 15:13:54 +0300

    Delete week2/src/ru/croc/task5 directory

--- week2/src/ru/croc/task5/Figure.java
+++
@@ -1,8 +0,0 @@
-package ru.croc.task5;
-
-public class Figure implements Movable  {
-    @Override
-    public void move(int dx, int dy) {
-        System.out.println("No move this annotatedImage");
-    }
-}
