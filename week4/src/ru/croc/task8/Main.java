68d47b831c799b198761a3cc8ec914d23da0600d
lumses <90564405+lumses@users.noreply.github.com>
2021-11-10 10:43:04 +0300

    Update Main.java

--- week4/src/ru/croc/task8/Main.java
+++ week4/src/ru/croc/task8/Main.java
@@ -1,18 +1,30 @@
 package ru.croc.task8;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 
 class Main {
     public static void main(String[] args) {
-        long count = 0;
-        try(Scanner scanner = new Scanner(new File(args[0]))) {
-            while (scanner.hasNext()) {
-                count++;
-                scanner.next();
+        int count = 0; // счетчик слов
+        int k = 0; // нужна для состояний 1-внутри слова, 0 - за пределами
+        String s ="./"+args[0];
+        BufferedReader reader; 
+        try { 
+            reader = new BufferedReader(new FileReader(s));
+            int c = reader.read();
+            while (c != -1) { // если выходим за пределы слова увеличиваем счетчик и делаем состояние за пределом
+                if (((char)c  >= 'а' && (char)c <= 'я') || ((char) c >= 'А' && (char) c <='Я')) {
+                    k = 1;
+                } else {
+                    if (k == 1) { 
+                        count++;
+                        k = 0;
+                    }
+                }
+                c = reader.read();
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             return;
         }
